3df537625cb2faf7c7fe69d52f59075753c5f8d3
Nathan <69707804+OneShot666@users.noreply.github.com>
2024-10-12 21:10:03 +1100

    v0.0.1
    Added project structure

---
+++ src/main.py
@@ -0,0 +1,52 @@
+from math import *
+from random import *
+from time import *
+
+
+# [v0.0.1] Structure of the project
+# ! [v0.0.2] Make basics functions
+# ! [v0.0.3] Create real database and test all functions
+# ! [v0.0.4] Make table a class (params as attributes) ?
+# ! [v1.0.0] Final tasks
+class Main:
+    def __init__(self):
+        # Project data
+        self.name = "DataGenerator"
+        self.creator = "One Shot"
+        self.version = "v0.0.1"
+        self.birthday = "12/10/12024"
+        # Database data
+        self.date_format = "%d%m%Y"
+        # Main functions
+        self.run()
+
+    def run(self):                                                              # Main function
+        pass
+
+    def check_data_correct(self):
+        pass
+
+    def access_database(self):
+        pass
+
+    def create_database(self):
+        pass
+
+    def remove_database(self):
+        pass
+
+    def add_table(self):
+        pass
+
+    def remove_table(self):
+        pass
+
+    def add_element(self):
+        pass
+
+    def remove_element(self):
+        pass
+
+
+if __name__ == "__main__":
+    main = Main()
